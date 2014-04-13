60c61721a9c4e6592a6ca16bfcc80f5402e9c672
Ruben Mendez <ruben.mendez@gmail.com>
2014-04-13 18:07:32 +0200

    messages

--- wp-content/themes/mystile-child/functions.php
+++ wp-content/themes/mystile-child/functions.php
@@ -31,4 +31,24 @@ function custom_post_box() {
 
 add_action( 'init', 'custom_post_box' );
 
+function box_updated_messages( $messages ) {
+	global $post, $post_ID;
+	$messages['box'] = array(
+		0 => '', 
+		1 => sprintf( __('Box updated. <a href="%s">View box</a>'), esc_url( get_permalink($post_ID) ) ),
+		2 => __('Custom field updated.'),
+		3 => __('Custom field deleted.'),
+		4 => __('Box updated.'),
+		5 => isset($_GET['revision']) ? sprintf( __('Box restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+		6 => sprintf( __('Box published. <a href="%s">View box</a>'), esc_url( get_permalink($post_ID) ) ),
+		7 => __('Box saved.'),
+		8 => sprintf( __('Box submitted. <a target="_blank" href="%s">Preview box</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+		9 => sprintf( __('Box scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview box</a>'), date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+		10 => sprintf( __('Box draft updated. <a target="_blank" href="%s">Preview box</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+	);
+	return $messages;
+}
+
+add_filter( 'post_updated_messages', 'box_updated_messages' );
+
 ?>
